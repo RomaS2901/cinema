ad663e56a4f9878dd2bd4d71d0eb4470348f1d56
romanbondarenko <romaha2901@gmail.com>
2023-03-23 22:57:40 +0200

    Users
    Changed user models email field to be used for auth
    Migration added

--- users/models.py
+++ users/models.py
@@ -1,5 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    email = models.EmailField(
+        unique=True,
+    )
