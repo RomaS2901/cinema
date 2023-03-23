4cf8095355bee87cb2c67a610c66d3ef739f45f1
romanbondarenko <romaha2901@gmail.com>
2023-03-23 22:57:02 +0200

    Swagger
    Added config

--- cinema/urls.py
+++ cinema/urls.py
@@ -15,6 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 from cinema.routers import router
 
@@ -22,6 +27,18 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path(
         "api/",
-        include(router.urls),
+        include(router_urlpatterns),
+    ),
+    # Docs
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
 ]
