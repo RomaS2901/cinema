d3d3e99c815b6b7c6054a131c4d303c42554552d
romanbondarenko <romaha2901@gmail.com>
2023-03-24 10:25:43 +0200

    Screening
    Movie CRUD added

--- screening/views.py
+++ screening/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from screening.models import Movie
+from screening.serializers import MovieModelSerializer
+
+
+class MovieViewSet(
+    CreateModelMixin,
+    GenericViewSet,
+):
+    serializer_class = MovieModelSerializer
+    queryset = Movie.objects.all()
+    permission_classes = [
+        IsAdminUser,
+    ]
